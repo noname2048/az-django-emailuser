4211c469065ad9ddd55806e8c53bbae97989186b
noname2048 <sungwook.csw@gmail.com>
2021-10-25 22:37:46 +0900

    feat: add pydenticon url, google login meta info

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path, include
 import django_pydenticon.urls
+from django.conf.urls import url
+from django_pydenticon.views import image as pydenticon_image
 
 app_name = "accounts"
 
 urlpatterns = [
-    re_path(r"^identicon/", include(django_pydenticon.urls.get_patterns())),
+    path("identicon/<path:data>", pydenticon_image, name="identicon"),
 ]
--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
