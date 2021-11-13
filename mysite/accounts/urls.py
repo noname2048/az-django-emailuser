8a4123229d3ccbe2d1e3efbffe1b8c21bb8835f3
noname2048 <sungwook.csw@gmail.com>
2021-11-13 23:52:13 +0900

    feat: add login, about_user urls, html

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path, include
 import django_pydenticon.urls
 from django.conf.urls import url
 from django_pydenticon.views import image as pydenticon_image
-from .views import signup_view, welcome_view
+from .views import signup_view, welcome_view, login_view, about_user_view
 
 
 app_name = "accounts"
@@ -11,4 +11,6 @@ urlpatterns = [
     path("identicon/<path:data>", pydenticon_image, name="identicon"),
     path("signup", signup_view, name="signup"),
     path("welcome", welcome_view, name="welcome"),
+    path("login", login_view, name="login"),
+    path("about/user/<int:id>", about_user_view, name="about_user"),
 ]
