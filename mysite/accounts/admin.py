b39e80d2b1b660b71813874e836a4358ee89d636
noname2048 <sungwook.csw@gmail.com>
2021-11-16 23:43:10 +0900

    feat: show is_staff display in MyUserAdmin

--- mysite/accounts/admin.py
+++ mysite/accounts/admin.py
@@ -4,5 +4,5 @@ from .models import MyUser
 
 @admin.register(MyUser)
 class MyUserAdmin(admin.ModelAdmin):
-    list_display = ["id", "email", "nickname", "date_joined"]
+    list_display = ["id", "email", "is_staff", "nickname", "date_joined"]
     list_display_links = ["email"]
